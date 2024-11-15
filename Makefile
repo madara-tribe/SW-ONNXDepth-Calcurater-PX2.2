988157928f49d34a232afd55b5cbc788743ee0fe
madara-tribe <tsukuba.univer@gmail.com>
2024-11-16 00:36:20 +0900

    update

--- Makefile
+++ Makefile
@@ -4,28 +4,9 @@ CMAKE_ARGS:=$(CMAKE_ARGS)
 USE_GPU=OFF
 
 default:
-	@mkdir -p build
-	@cd build && cmake .. -DBUILD_EXAMPLES=$(BUILD_EXAMPLES) \
-                              -DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
-                              -DUSE_GPU=$(USE_GPU) \
-                              -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-                              $(CMAKE_ARGS)
-	@cd build && make
-
-gpu_default:
-	@make default USE_GPU=ON
-
-debug:
-	@make default BUILD_TYPE=Debug
-
-apps:
-	@make default BUILD_EXAMPLES=ON
-
-gpu_apps:
-	@make apps USE_GPU=ON
-
-debug_apps:
-	@make debug BUILD_EXAMPLES=ON
+	@cmake -B build
+	@cmake --build build --config Release --parallel
+	@cd build/src && ./inference 
 
 clean:
 	@rm -rf build*
--- src/inference.cpp
+++ src/inference.cpp
@@ -3,30 +3,50 @@
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 #include <fstream>
 #include <vector>
-#define ONNX_MODEL_PATH "dpt_large_384.onnx"
-#define IMG_PATH "input/input.jpg"
+#include <opencv2/dnn/dnn.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
-int onnx_predict(bool useCUDA){
-    // Define data and model
-    std::string instanceName{"cycleGAN inference"};
-    std::string modelFilepath{ONNX_MODEL_PATH};
-    // std::string imageFilepath{IMG_PATH};
-    
-    Ort::Env env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING,
-                 instanceName.c_str());
-    Ort::SessionOptions sessionOptions;
-    sessionOptions.SetIntraOpNumThreads(1);
-    if (useCUDA)
-    {
-    OrtStatus* status =
-            OrtSessionOptionsAppendExecutionProvider_CUDA(sessionOptions, 0);
-    
-    return 0;
+#define ONNX_DEPTH_PATH "/workspace/weights/dpt_large_384.onnx"
+#define ONNX_YOLO_PATH "/workspace/weights/yolov7Tiny_640_640.onnx"
+#define IMG_PATH "/workspace/input/input.jpg"
+
+
+int model_check() {
+    // Initialize the ONNX Runtime environment
+    Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNX_Model");
+    std::string modelFilepath{ONNX_DEPTH_PATH};
+    std::ifstream modelFile(modelFilepath);
+    if (!modelFile.good()) {
+        std::cerr << "Model file does not exist or could not be found: " << modelFilepath << std::endl;
+    return -1;
     }
+    // Create an ONNX Runtime session options object
+    Ort::SessionOptions session_options;
+    session_options.SetIntraOpNumThreads(1);
+    session_options.SetGraphOptimizationLevel(
+        GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
+    // Load the model
+    std::cout << "model name " << modelFilepath.c_str() << std::endl;
+    Ort::Session session(env, modelFilepath.c_str(), session_options);
+    // Get input and output details
+    size_t num_input_nodes = session.GetInputCount();
+    Ort::AllocatorWithDefaultOptions allocator;
+
+    for (size_t i = 0; i < num_input_nodes; i++) {
+        // Updated to use GetInputNameAllocated
+        Ort::AllocatedStringPtr input_name = session.GetInputNameAllocated(i, allocator);
+        std::cout << "Input " << i << ": " << input_name.get() << std::endl;
+    }
+    std::cout << "Model loaded successfully!" << std::endl;
+    return 0;
+}
+
+
 
 int main(int argc, char* argv[])
 {
-    bool useCUDA{false};
+    bool useCUDA{true};
     const char* useCUDAFlag = "--use_cuda";
     const char* useCPUFlag = "--use_cpu";
     if ((argc == 2) && (strcmp(argv[1], useCUDAFlag) == 0))
@@ -39,6 +59,6 @@ int main(int argc, char* argv[])
         useCUDA = false;
         std::cout << "Inference Execution Provider: CPU" << std::endl;
     }
-
-    return onnx_predict(useCUDA);
+   
+    return model_check(); //(useCUDA);
 }
