862ec6aaf9992aeae79de4f0cac2add43dd53b5c
madara-tribe <tsukuba.univer@gmail.com>
2024-11-23 22:56:05 +0900

    uodate

--- src/inference.cpp
+++ src/inference.cpp
@@ -1,9 +1,32 @@
 #include <chrono>
+#include "yolo_inference.h"
+#include "midas_inference.h"
 
 #define ONNX_DEPTH_PATH "/workspace/weights/dpt_large_384.onnx"
-#define IMG_PATH "/workspace/data/indoor.jpg"
+#define MIDAS_IMG_PATH "/workspace/data/indoor.jpg"
+#define ONNX_YOLO_PATH "/workspace/weights/yolov7Tiny_640_640.onnx"
+#define YOLO_IMG_PATH "/workspace/data/dog.jpg"
+#define YOLO_INPUT_H 640
+#define YOLO_INPUT_W 640
 
-#include "midas_inference.h"
+
+int yolo(){
+    std::string model_path = ONNX_YOLO_PATH;
+    YoloDetect yolo_detector(model_path);
+    
+    cv::Mat image = cv::imread(YOLO_IMG_PATH);
+    cv::Mat inputImage = yolo_detector.preprocess(image, YOLO_INPUT_H, YOLO_INPUT_W);
+
+    
+    std::vector<Ort::Value> outputTensors = yolo_detector.RunInference(inputImage);
+
+    std::vector<Result> resultVector = yolo_detector.postprocess(image.size(), outputTensors);
+
+    yolo_detector.drawBoundingBox(image, resultVector);
+    cv::imwrite("resultt.png", image);
+
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
     bool useCUDA{false};
@@ -24,7 +47,8 @@ int main(int argc, char* argv[]) {
     MidasInference midas(onnx_model_path, useCUDA);
     
     auto start = std::chrono::high_resolution_clock::now();
-    midas.runInference(IMG_PATH);
+    midas.runInference(MIDAS_IMG_PATH);
+    yolo();
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end - start;
     std::cout << "Prediction took " << diff.count() << " seconds" << std::endl;
